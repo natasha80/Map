56ab0e5a0c72a40209f6ff8568f32cf193cef06d
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-08-09 13:37:25 +0700

    initial commit

--- src/js/ErrorRepository.js
+++ src/js/ErrorRepository.js
@@ -1,9 +1,9 @@
 export default class ErrorRepository {
-    constructor() {
-      this.errStorage = new Map();
-    }
-  
-    translate(code) {
-      return this.errStorage.get(code) || 'Unknown error';
-    }
+  constructor() {
+    this.errStorage = new Map();
+  }
+
+  translate(code) {
+    return this.errStorage.get(code) || 'Unknown error';
+  }
 }
--- src/js/__test__/ErrorRepository.test.js
+++ src/js/__test__/ErrorRepository.test.js
@@ -7,34 +7,44 @@ const errors = [
   [1004, 'Такой персонаж уже eсть в команде'],
 ];
 
+// eslint-disable-next-line no-undef
 test('Метод translate должен вернуть значение по ключу', () => {
   const Test = new ErrorRepository();
   Test.errStorage.set(1000, 'test error');
+  // eslint-disable-next-line no-undef
   expect(Test.translate(1000)).toBe('test error');
 });
 
+// eslint-disable-next-line no-undef
 test('Метод translate должен вернуть Unknown error, если ключа нет', () => {
   const Test = new ErrorRepository();
   Test.errStorage.set(1000, 'test error');
+  // eslint-disable-next-line no-undef
   expect(Test.translate(2000)).toBe('Unknown error');
 });
 
+// eslint-disable-next-line no-undef
 test('Переданный в инстанс массив ошибок должен сохраниться в хранилище', () => {
   const Test = new ErrorRepository();
   Test.errStorage = new Map(errors);
+  // eslint-disable-next-line no-undef
   expect(Test.errStorage.size).toBe(4);
 });
 
+// eslint-disable-next-line no-undef
 test('Передали массив ошибок и 1 ошибку отдельно, должны сохраниться все (5 шт)', () => {
   const Test = new ErrorRepository();
   Test.errStorage = new Map(errors);
   Test.errStorage.set(1000, 'Test error');
+  // eslint-disable-next-line no-undef
   expect(Test.errStorage.size).toBe(5);
 });
 
+// eslint-disable-next-line no-undef
 test('Передали массив ошибок и один дубль отдельно, дубль должен перезаписаться', () => {
   const Test = new ErrorRepository();
   Test.errStorage = new Map(errors);
   Test.errStorage.set(1001, 'Errror test');
+  // eslint-disable-next-line no-undef
   expect(Test.errStorage.size).toBe(4);
 });
